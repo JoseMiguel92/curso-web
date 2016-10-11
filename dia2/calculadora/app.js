fd51b58e28f11bf3bfb92b12f3215b358f2d3a2d
Jose Miguel Garcia <jmgbenayas@gmail.com>
2016-10-11 18:00:35 +0200

    Finalizacion de la calculadora basica

--- dia2/calculadora/app.js
+++ dia2/calculadora/app.js
@@ -1,3 +1,9 @@
+var display;
+var zero = true;
+var valor = 0;
+var opt;
+var dec= true;
+
 function number(x){
     switch(x){
         case 9:
@@ -31,11 +37,61 @@ function number(x){
             addNumber(0);
             break;
         default:
+            break;
     }
 }
+
 function addNumber(x){
-    var display = document.getElementById("pantalla");
-    if(!(display.value.length >= 9)){
-       display.value += x;
+    display = document.getElementById("pantalla");
+    if(x==0 && !zero){
+        display.value += x;
     }
+    else if(x!=0){
+        zero = false;
+        display.value += x;
+    }
+}
+
+function operacion(op){
+    valor = document.getElementById("pantalla").value;
+    opt = op;
+    valor = document.getElementById("pantalla").value = "";
+    zero = true;
+    switch(opt){
+        case "+":
+
+            break;
+        case "+":
+
+            break;
+        case "*":
+
+            break;
+        case "/":
+
+            break;
+        case "/":
+
+            break;
+        default: break;
+    }
+}
+
+function resultdo(){
+    var display = document.getElementById("pantalla");
+}
+
+function decimal(){
+    if(zero){
+        document.getElementById("pantalla").value="0.";
+    }else if(dec){
+        document.getElementById("pantalla").value += ".";
+    }
+    dec = false;
+}
+
+function CE(){
+    document.getElementById("pantalla").value="";
+    dec=true;
+    zero=true;
 }
